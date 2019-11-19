a641daf7e9bd7d85fbba19e37828e13d00083b85
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2019-11-18 16:12:04 -0800

    Perform installs concurrently

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/zikaeroh/gotools
 
 go 1.12
 
-require golang.org/x/mod v0.1.1-0.20191105210325-c90efee705ee
+require (
+	golang.org/x/mod v0.1.1-0.20191105210325-c90efee705ee
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+)
--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
 
 	"golang.org/x/mod/modfile"
+	"golang.org/x/sync/semaphore"
 )
 
 var configDir = getConfigDir()
@@ -23,6 +26,7 @@ var (
 	fMods        = flag.String("mods", configDirFor("mods"), "module configuration directory")
 	fVersions    = flag.String("versions", configDirFor("versions"), "versions output file")
 	fCopyReplace = flag.Bool("copyreplace", true, "copy replacements from tool's go.mod")
+	fWorkers     = flag.Int64("workers", int64(runtime.GOMAXPROCS(0))+1, "number of concurrent workers")
 )
 
 func main() {
@@ -30,15 +34,9 @@ func main() {
 
 	f, err := os.Open(*fConfig)
 	if err != nil {
-		panic(err)
+		log.Fatal("error opening config", err)
 	}
 	defer f.Close()
-
-	vf, err := os.Create(*fVersions)
-	if err != nil {
-		panic(err)
-	}
-	defer vf.Close()
 
 	scanner := bufio.NewScanner(f)
 
@@ -62,24 +60,56 @@ func main() {
 	}
 
 	if err := scanner.Err(); err != nil {
-		panic(err)
+		log.Fatal("config scanner error", err)
 	}
 
 	if curr != nil {
 		tools = append(tools, curr)
 	}
 
-	for _, t := range tools {
-		t.run(vf)
+	versions := make([]string, len(tools))
+
+	sem := semaphore.NewWeighted(*fWorkers)
+
+	for i := range tools {
+		if err := sem.Acquire(context.Background(), 1); err != nil {
+			log.Println(err)
+			return
+		}
+
+		go func(i int) {
+			defer sem.Release(1)
+
+			t := tools[i]
+
+			if err := t.run(); err != nil {
+				log.Println(err)
+			}
+			versions[i] = t.name + " " + t.finalVer
+		}(i)
+	}
+
+	vf, err := os.Create(*fVersions)
+	if err != nil {
+		log.Fatal("error creating version file", err)
+	}
+	defer vf.Close()
+
+	for _, ver := range versions {
+		if _, err := fmt.Fprintln(vf, ver); err != nil {
+			log.Fatal("error writing to version file", err)
+		}
 	}
 }
 
 type tool struct {
 	name   string
 	verReq string
-	tmpMod string
+	wd     workingDir
 	tags   string
 	setup  []string
+
+	finalVer string
 }
 
 func newTool(name string) *tool {
@@ -92,7 +122,7 @@ func newTool(name string) *tool {
 	return &tool{
 		name:   name,
 		verReq: ver,
-		tmpMod: filepath.Join(*fMods, mod),
+		wd:     workingDir(filepath.Join(*fMods, mod)),
 	}
 }
 
@@ -113,26 +143,48 @@ func (t *tool) parse(line string) (next bool) {
 	return false
 }
 
-func (t *tool) run(vOut io.Writer) {
-	mkdirCd(t.tmpMod)
+func (t *tool) run() error {
+	if err := t.wd.mkdir(); err != nil {
+		return err
+	}
 
-	hasGoMod := exists("go.mod")
+	hasGoMod := t.wd.contains("go.mod")
 
 	var oldVer string
 	if hasGoMod {
-		oldVer = t.version()
+		var err error
+		oldVer, err = t.version()
+		if err != nil {
+			return err
+		}
 	}
 
 	t.writeToolsGo()
 
 	if *fUpdate || !hasGoMod {
-		rm("go.mod")
-		rm("go.sum")
-		run("go", "mod", "init", "tmpmod")
-		run("go", "get", "-d", t.name+"@"+t.verReq)
+		if err := t.wd.rm("go.mod"); err != nil {
+			return err
+		}
+
+		if err := t.wd.rm("go.sum"); err != nil {
+			return err
+		}
+
+		if _, err := t.wd.run("go", "mod", "init", "tmpmod"); err != nil {
+			return err
+		}
+
+		if _, err := t.wd.run("go", "get", "-d", t.name+"@"+t.verReq); err != nil {
+			return err
+		}
 
 		if *fCopyReplace {
-			if toolMod := run("go", "list", "-f", `{{.Module.GoMod}}`, t.name); toolMod != "" {
+			toolMod, err := t.wd.run("go", "list", "-f", `{{.Module.GoMod}}`, t.name)
+			if err != nil {
+				return err
+			}
+
+			if toolMod != "" {
 				data, err := ioutil.ReadFile(toolMod)
 				if err != nil {
 					panic(err)
@@ -149,37 +201,51 @@ func (t *tool) run(vOut io.Writer) {
 					}
 
 					replacement := fmt.Sprintf("%s=%s@%s", replace.Old.Path, replace.New.Path, replace.New.Version)
-					run("go", "mod", "edit", "-replace", replacement)
+
+					if _, err := t.wd.run("go", "mod", "edit", "-replace", replacement); err != nil {
+						return err
+					}
 				}
 			}
 		}
 
 		for _, cmdline := range t.setup {
-			run("sh", "-c", cmdline)
+			if _, err := t.wd.run("sh", "-c", cmdline); err != nil {
+				return err
+			}
 		}
 
-		run("go", "mod", "tidy")
+		if _, err := t.wd.run("go", "mod", "tidy"); err != nil {
+			return err
+		}
 	}
 
-	t.install()
+	if err := t.install(); err != nil {
+		return err
+	}
 
-	ver := t.version()
-
-	fmt.Fprintf(vOut, "%s %s\n", t.name, ver)
+	ver, err := t.version()
+	if err != nil {
+		return err
+	}
 
 	if oldVer == "" || oldVer == ver {
 		fmt.Printf("%s %s\n", t.name, ver)
 	} else {
 		fmt.Printf("%s %s -> %s\n", t.name, oldVer, ver)
 	}
+
+	t.finalVer = ver
+	return nil
 }
 
-func (t *tool) install() {
-	run("go", "install", "-tags", t.tags, t.name)
+func (t *tool) install() error {
+	_, err := t.wd.run("go", "install", "-tags", t.tags, t.name)
+	return err
 }
 
-func (t *tool) version() string {
-	return run("go", "list", "-f", "{{.Module.Version}}", t.name)
+func (t *tool) version() (string, error) {
+	return t.wd.run("go", "list", "-f", "{{.Module.Version}}", t.name)
 }
 
 const toolsGo = `// +build tools
@@ -190,11 +256,11 @@ import _ "%s"
 `
 
 func (t *tool) writeToolsGo() {
-	if exists("tools.go") {
+	if t.wd.contains("tools.go") {
 		return
 	}
 
-	f, err := os.Create("tools.go")
+	f, err := t.wd.create("tools.go")
 	if err != nil {
 		panic(err)
 	}
@@ -221,41 +287,46 @@ func splitFirstSep(s string, sep string) (l string, r string) {
 	}
 }
 
-func run(name string, args ...string) string {
+type workingDir string
+
+func (w workingDir) run(name string, args ...string) (string, error) {
 	cmd := exec.Command(name, args...)
+	cmd.Dir = string(w)
+
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 
 	if err := cmd.Run(); err != nil {
-		fmt.Println(cmd)
-		fmt.Printf("STDOUT\n%s", stdout.String())
-		fmt.Printf("STDERR\n%s", stderr.String())
-		panic(err)
+		log.Printf("%s\nSTDOUT\n%s\nSTDERR\n%s", cmd, stdout.String(), stderr.String())
+		return "", err
 	}
 
-	return strings.TrimSpace(stdout.String())
+	return strings.TrimSpace(stdout.String()), nil
 }
 
-func mkdirCd(path string) {
-	if err := os.MkdirAll(path, 0700); err != nil {
-		panic(err)
-	}
-	if err := os.Chdir(path); err != nil {
-		panic(err)
-	}
-}
-
-func exists(file string) bool {
-	_, err := os.Stat(file)
+func (w workingDir) contains(filename string) bool {
+	filename = filepath.Join(string(w), filename)
+	_, err := os.Stat(filename)
 	return err == nil
 }
 
-func rm(file string) {
-	if err := os.Remove(file); err != nil && !os.IsNotExist(err) {
-		panic(err)
+func (w workingDir) mkdir() error {
+	return os.MkdirAll(string(w), 0700)
+}
+
+func (w workingDir) rm(filename string) error {
+	filename = filepath.Join(string(w), filename)
+	if err := os.Remove(filename); err != nil && !os.IsNotExist(err) {
+		return err
 	}
+	return nil
+}
+
+func (w workingDir) create(filename string) (*os.File, error) {
+	filename = filepath.Join(string(w), filename)
+	return os.Create(filename)
 }
 
 func getConfigDir() string {
